48a891802c609d904d163c329cb92bb41b93c3ba
Timimah <44163985+Timimah@users.noreply.github.com>
2023-05-15 23:08:48 +0100

    updated button

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -16,15 +16,12 @@ export const Signup = () => {
             <img src={join} alt="" srcset="" />
           </div>
           <div className="flex flex-col md:w-1/2 px-10 py-10 gap-6 my-auto">
-            <div className="flex place-content-center gap-4 py-2 px-10 bg-button font-semibold rounded-lg hover:bg-transparent border-2 border-button mt-4 cursor-pointer">
               {/* to="http://localhost:8080/auth/google" */}
-
-              <Link to="https://salva-vjxd.onrender.com/auth/google" className="flex place-content-center gap-4 py-2 px-10 bg-button font-semibold rounded-lg hover:bg-transparent border-2 border-button mt-4 cursor-pointer">
+            <Link to="https://salva-vjxd.onrender.com/auth/google" className="flex place-content-center gap-4 py-2 px-10 bg-button font-semibold rounded-lg hover:bg-transparent border-2 border-button mt-4 cursor-pointer">
               {/* <Link to="http://localhost:8080/auth/google"> */}
                 <Button title={<FaGoogle />} btnstyles="py-3 rounded-full" />
                 <Button title="Create an Account" btnstyles="" />
-              </Link>
-            </div>
+            </Link>
             <div className="flex place-content-center gap-4 py-2 px-10 bg-button font-semibold rounded-lg hover:bg-transparent border-2 border-button mt-4 cursor-pointer">
               <Button title={<FaTwitter />} btnstyles="py-3 rounded-full" />
               <Button title="Create an Account" btnstyles="" />
