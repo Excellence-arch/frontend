abfb782b9bb72337ed23619999477dc77a513442
Timimah <44163985+Timimah@users.noreply.github.com>
2023-05-15 10:30:07 +0100

    replaced forms with social authentication

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -13,20 +13,20 @@ export const Login = () => {
             <Layout>
                 <div className='px-10 py-10'>
                     <div className='flex flex-col gap-6 md:w-4/6 md:mx-auto md:justify-center'>
-                    <div className='text-4xl font-bold mb-6'>Log in</div>
-                    <input className='p-4 border-2 border-primary text-primary rounded-lg' type="text" placeholder="Username/Email Address" name="" />
-                    <input className='p-4 border-2 border-primary text-primary rounded-lg' type="password" placeholder="Password" name="" />
-                    <Button title="Log In" btnstyles="py-3 bg-button font-semibold rounded-lg hover:bg-transparent border-2 border-button mt-4" />
-                    <div className="text-center mt-4">
-                        <div className='border-b border-paragraph -mb-3'></div>
-                        <div className='bg-gradient-to-l from-[#272d4c] to-[#2a3051] px-2 inline-block'>Log In with</div>
-                    </div>
-                    <div className='flex justify-center gap-4'>
-                        <Button title={<FaGoogle />} btnstyles="py-3 rounded-full text-button hover:text-white" />
-                        <Button title={<FaTwitter />} btnstyles="py-3 rounded-full text-button hover:text-white" />
-                        <Button title={<FaFacebookF />} btnstyles="py-3 rounded-full text-button hover:text-white" />
-                    </div>
-                    <div className='text-center'>Don't have an account? <Link to="/signup" className='hover:text-button'>Register</Link></div>
+                        <div className='text-4xl font-bold mb-6'>Log in</div>
+                        <div className='flex place-content-center gap-4 py-2 px-10 bg-button font-semibold rounded-lg hover:bg-transparent border-2 border-button mt-4 cursor-pointer'>
+                            <Button title={<FaGoogle />} btnstyles="py-3 rounded-full" />
+                            <Button title="Log In" btnstyles="" />
+                        </div>
+                        <div className='flex place-content-center gap-4 py-2 px-10 bg-button font-semibold rounded-lg hover:bg-transparent border-2 border-button mt-4 cursor-pointer'>
+                            <Button title={<FaTwitter />} btnstyles="py-3 rounded-full" />
+                            <Button title="Log In" btnstyles="" />
+                        </div>
+                        <div className='flex place-content-center gap-4 py-2 px-10 bg-button font-semibold rounded-lg hover:bg-transparent border-2 border-button mt-4 cursor-pointer'>
+                            <Button title={<FaFacebookF />} btnstyles="py-3 rounded-full" />
+                            <Button title="Log In" btnstyles="" />
+                        </div>
+                        <div className='text-center'>Don't have an account? <Link to="/signup" className='hover:text-button'>Register</Link></div>
                     </div>
                 </div>
             </Layout>
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -16,24 +16,18 @@ export const Signup = () => {
                     <div className='md:w-1/2 px-10 place-items-center flex'>
                         <img src={join} alt="" srcset="" />
                     </div>
-                    <div className='flex flex-col md:w-1/2 px-10 py-10 gap-6'>
-                        <div className='flex flex-col gap-6 md:flex-row'>
-                            <input className='p-4 md:mr-2 w-full border-2 border-primary text-primary rounded-lg' type="text" placeholder="First Name" name="" id="" />
-                            <input className='p-4 md:ml-2 w-full border-2 border-primary text-primary rounded-lg' type="text" placeholder="Last Name" name="" id="" />
+                    <div className='flex flex-col md:w-1/2 px-10 py-10 gap-6 my-auto'>
+                        <div className='flex place-content-center gap-4 py-2 px-10 bg-button font-semibold rounded-lg hover:bg-transparent border-2 border-button mt-4 cursor-pointer'>
+                            <Button title={<FaGoogle />} btnstyles="py-3 rounded-full" />
+                            <Button title="Log In" btnstyles="" />
                         </div>
-                        <input className='p-4 border-2 border-primary text-primary rounded-lg' type="text" placeholder="Username" name="" id="" />
-                        <input className='p-4 border-2 border-primary text-primary rounded-lg' type="email" placeholder="Email address" name="" id="" />
-                        <input className='p-4 border-2 border-primary text-primary rounded-lg' type="password" placeholder="Password" name="" id="" />
-                        <input className='p-4 border-2 border-primary text-primary rounded-lg' type="password" placeholder="Confirm password" name="" id="" />
-                        <Button title="Sign Up" btnstyles="py-3 bg-button font-semibold rounded-lg hover:bg-transparent border-2 border-button hover:text-white mt-4" />
-                        <div className="text-center mt-4">
-                        <div className='border-b border-paragraph -mb-3'></div>
-                        <div className='bg-gradient-to-l from-[#272d4c] to-[#2a3051] px-2 inline-block'>Or Sign Up using</div>
-                    </div>
-                        <div className='flex justify-center gap-4'>
-                            <Button title={<FaGoogle/>} btnstyles="py-3 rounded-full text-button hover:text-white" />
-                            <Button title={<FaTwitter />} btnstyles="py-3 rounded-full text-button hover:text-white" />
-                            <Button title={<FaFacebookF />} btnstyles="py-3 rounded-full text-button hover:text-white" />
+                        <div className='flex place-content-center gap-4 py-2 px-10 bg-button font-semibold rounded-lg hover:bg-transparent border-2 border-button mt-4 cursor-pointer'>
+                            <Button title={<FaTwitter />} btnstyles="py-3 rounded-full" />
+                            <Button title="Log In" btnstyles="" />
+                        </div>
+                        <div className='flex place-content-center gap-4 py-2 px-10 bg-button font-semibold rounded-lg hover:bg-transparent border-2 border-button mt-4 cursor-pointer'>
+                            <Button title={<FaFacebookF />} btnstyles="py-3 rounded-full" />
+                            <Button title="Log In" btnstyles="" />
                         </div>
                         <div>Already have an account? <Link to="/login" className='text-white hover:text-button'>Log in</Link></div>
                     </div>
