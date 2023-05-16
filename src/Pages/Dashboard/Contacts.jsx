4d32b625875dde8b1c021cd692a298495b3b90da
Timimah <44163985+Timimah@users.noreply.github.com>
2023-05-16 16:07:08 +0100

    cretaed emergency contact page

---
+++ src/Pages/Dashboard/Contacts.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+import { Button } from '../../components/Button'
+import { Header } from './Header'
+
+export const Contacts = () => {
+  const [showForm, setshowForm] = useState(false);
+  const [contacts, setContacts] = useState([]);
+  const [name, setName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+
+  const toggleForm = () => {
+    setshowForm(!showForm);
+  }
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handlePhoneNumberChange = (event) => {
+    setPhoneNumber(event.target.value);
+  };
+
+  const handleAddContact = () => {
+    const newContact = {
+      name: name,
+      phoneNumber: phoneNumber,
+    };
+
+    setContacts([...contacts, newContact]);
+    setName('');
+    setPhoneNumber('');
+    // setShowForm(false); // Hide the form after saving the contact
+  };
+
+  return (
+    <div>
+      <Header />
+      <div className="h-screen flex flex-col gap-6 justify-center items-center">
+        {showForm ? <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50'>
+          <div className='bg-paragraph w-2/3 rounded-lg py-10'>
+            <div className='flex flex-col gap-6 w-full px-5 md:px-10'>
+              <div>Add New contact</div>
+              <input className='p-4 border-2 border-primary text-primary rounded-lg'
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={handleNameChange}
+              />
+              <input className='p-4 border-2 border-primary text-primary rounded-lg'
+                type="text"
+                placeholder="Phone Number"
+                value={phoneNumber}
+                onChange={handlePhoneNumberChange}
+              />
+              <Button title="Save" btnstyles="py-3 bg-button font-semibold rounded-lg hover:bg-transparent border-2 border-button mt-4" onClick={handleAddContact}
+              />
+              <Button
+                title="Cancel"
+                btnstyles="py-3 bg-button font-semibold rounded-lg border-2 border-button mt-4"
+                onClick={() => {console.log(showForm)}}
+              />
+            </div>
+          </div>
+        </div>
+          : <div>
+            <div className='text-3xl'>No Emergency contacts</div>
+            <div>
+              <Button title='+' btnstyles='hover:text-button hover:bg-white border text-center py-3 px-6 rounded-full text-4xl' onclick={toggleForm} />
+            </div>
+          </div>}
+      </div>
+    </div>
+  )
+}
